98ce6940d113590af9efaa66ac8f3b0e714737f0
adceballos <78570037+adceballos@users.noreply.github.com.]>
2024-08-06 19:19:08 -0500

    Create new section that displays a title and header for the workout generator
    - Create a new SectionWrapper.jsx component and pass props to its function through the Generator component
    - Deconstruct props within SectionWrapper and style them using html and css classes
    - Also made a small change to Hero.jsx by giving the outer div a padding of 4

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import SectionWrapper from './SectionWrapper'
 
 export default function Generator() {
   return (
-    <div>Generator</div>
-  )
+    // Pass header and title as attribute style props to SectionWrapper component.
+    <SectionWrapper header={"generate your workout"} title={['Time', 'to', 'flex', 'those', 'Claws']}>
+
+    </SectionWrapper>
+      )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto'>
+    <div className='min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] w-full mx-auto p-4'>
       <div className='flex flex-col gap-4'>
         <p>GET READY TO DIVE <span>DEEP</span></p>
         <h1 className='font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'><span className='text-blue-400'>MANTIS</span> FITNESS</h1>
