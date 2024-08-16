8c6eff28e6d09912f92d634d205d17330a68d8ea
adceballos <78570037+adceballos@users.noreply.github.com.]>
2024-08-15 23:28:26 -0500

    Add state management for modal visibility, workout type, selected muscles, and fitness goals.

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import SectionWrapper from './SectionWrapper'
 import { SCHEMES, WORKOUTS } from '../utils/swoldier'
 
@@ -17,11 +17,22 @@ function Header(props) {
 }
 
 export default function Generator() {
+  // We import {useState} to use a React stateful variable instead of a traditional variable. First value is the current state, and second value is the function that is used to update our state. useState is the hook that is declared with a default value.
+  const [showModal, setShowModal] = useState(false)
+  const [poison, setPoison] = useState('individual')
+  const [muscles, setMuscles] = useState([])
+  const [goals, setGoals] = useState('strength_power')
+
+  function toggleModal() {
+    setShowModal(!showModal)
+    //showModal = !showModal
+    }
+
   return (
     // Pass header and title as attribute style props to SectionWrapper component.
     <SectionWrapper header={"generate your workout"} title={['Time', 'to', 'flex', 'those', 'Claws']}>
         {/* In JSX, you can use a self-closing tag if a component does not have any children. <Header /> acts the same as <Header></Header>. */}
-        <Header index={'01'} title={'Choose your challenge'} description={"selection the workout you wish to endure."} />
+        <Header index={'01'} title={'Pick your poison'} description={"selection the workout you wish to endure."} />
         {/* Retrieve an array of keys from WORKOUTS object located in swoldier.js, then map each key as a button, representing 4 different workout splits.
             In the arrow function, type represents the current key, and type index represents the index of that key in the array. */}
         <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
@@ -35,12 +46,16 @@ export default function Generator() {
           })}
         </div>
         <Header index={'02'} title={'Lock on Targets'} description={"selection the muscles judged for annihilation."} />
-        <div className='bg-slate-950 p-3 border border-solid border-blue-400 rounded-lg'>
-          <div className='relative flex items-center justify-center'>
+        <div className='bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col'>
+          <button onClick={toggleModal} className='relative flex items-center justify-center p-3'>
             <p>Select muscle groups</p>
-            {/* Carrot down icon imported from fontawesome */}
+            {/* Carrot down icon imported from fontawesome, then moved it to the right of our box. */}
             <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2"></i>
-          </div>
+          </button>
+          { /* Conditional rendering section. Use short circuit operator if show model == true */ }
+          {showModal && (
+            <div>modal</div>
+          )}
         </div>
     </SectionWrapper>
   )
